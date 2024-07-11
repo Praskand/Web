ebf0bc62f035f9b292e1c57864bb76bd04a0849d
Praskand <95966972+Praskand@users.noreply.github.com>
2024-07-11 16:36:56 +0530

    1 0 1

--- 3.js
+++ 3.js
@@ -1,7 +1,7 @@
 isReverseNumber = (number) => {ns = number.toString();
     return ns.split('').reverse().join('') === ns ?  1:0;}
-    console.log(isReverseNumber(121), isReverseNumber(12), isReverseNumber(1));
-// 1 0 1
+    console.log(isReverseNumber(121), isReverseNumber(12), isReverseNumber(1)); // 1 0 1
+
 longest = (wordList) => { l = wordList[0].length, re = '';
 // console.log(l);
 	for(i = 1; i < wordList.length; i++) { wi = wordList[i]; if (l < wi.length) {
