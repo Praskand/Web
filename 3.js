c02df73b3afe6873f7d736b5c373a4f7331f7fc6
Praskand <95966972+Praskand@users.noreply.github.com>
2024-07-11 15:52:26 +0530

    rev

--- 3.js
+++ 3.js
@@ -1,1 +1,4 @@
-ns = number.toString(); if ns.split('').reverse().join('') === ns
+isReverseNumber = (number) => {ns = number.toString();
+    return ns.split('').reverse().join('') === ns?  1:0;}
+    console.log(isReverseNumber(121), isReverseNumber(12), isReverseNumber(1));
+// 1 0 1
